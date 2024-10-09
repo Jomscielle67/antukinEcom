d03cb53f16d834c6b77fdedac92d843c5f9a441d
antukinJoms <pamisjj@gmail.com>
2024-10-09 14:22:32 +0800

    antukin-V8-railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -91,30 +91,30 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         # 'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
-#         'PASSWORD': os.environ['DB_PASSWORD_YO'],
-#         'HOST': 'junction.proxy.rlwy.net',
-#         'PORT': '46361',
-#     }
-# } shet talaga
-
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'railway'),  # Defaults to 'railway' if not set
-        'USER': os.environ.get('DB_USER', 'postgres'),  # Defaults to 'postgres' if not set
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),   # Fetches from environment variables
-        'HOST': os.environ.get('DB_HOST', 'junction.proxy.rlwy.net'),  # Defaults to Railway host
-        'PORT': os.environ.get('DB_PORT', '46361'),  # Defaults to Railway port
+        'NAME': 'railway',
+        'USER': 'postgres',
+        # 'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '46361',
     }
-}
+} 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME', 'railway'),  # Defaults to 'railway' if not set
+#         'USER': os.environ.get('DB_USER', 'postgres'),  # Defaults to 'postgres' if not set
+#         'PASSWORD': os.environ.get('DB_PASSWORD_YO'),   # Fetches from environment variables
+#         'HOST': os.environ.get('DB_HOST', 'junction.proxy.rlwy.net'),  # Defaults to Railway host
+#         'PORT': os.environ.get('DB_PORT', '46361'),  # Defaults to Railway port
+#     }
+# }
 
 
 
